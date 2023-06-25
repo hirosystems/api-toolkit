4a2c8b334377dd79db9531030d229ecedc91debb
Rafael Cardenas <rafael@rafaelcr.com>
2023-06-25 15:27:32 -0600

    feat: git info bin

--- bin/api-toolkit-git-info.js
+++ bin/api-toolkit-git-info.js
@@ -1,8 +1,10 @@
-import { execSync } from 'child_process';
-import { writeFileSync } from 'fs';
+#!/usr/bin/env node
+
+const child_process = require('child_process');
+const fs = require('fs');
 
 try {
-  execSync('git --version');
+  child_process.execSync('git --version');
 } catch (error) {
   console.error(error.message);
   throw new Error(`git is missing, please install git and retry`);
@@ -13,11 +15,11 @@ const gitInfo = [
   'git describe --tags --abbrev=0',
 ].map((r, index) => {
   try {
-    return execSync(r, { encoding: 'utf8' }).trim();
+    return child_process.execSync(r, { encoding: 'utf8' }).trim();
   } catch (error) {
     console.error(error.message);
     if (index === 2) throw new Error(`no tag found fetch tags by running "git fetch --all --tags"`);
     throw error;
   }
 });
-writeFileSync('.git-info', gitInfo.join('\n'));
+fs.writeFileSync('.git-info', gitInfo.join('\n'));
