840b2f1d9378762db7985bbf9f06bb49b78a5b8e
Matthew Little <zone117x@gmail.com>
2025-06-02 16:12:21 +0700

    feat: onceWhen function (#43)
    * feat: implement `onceWhen` helper function
    
    * chore: add example usage docs
    
    * chore: add example usage docs

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,5 +2,6 @@ export * from './iterators';
 export * from './time';
 export * from './values';
 export * from './is-debugging';
+export * from './events';
 export { WorkerThreadManager } from './worker-thread-manager';
 export type { WorkerPoolModuleInterface } from './worker-thread-manager';
